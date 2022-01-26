e9b72f66ed15cc374c76a37ae6db0ee1183379ac
timegated <beccdaniel@gmail.com>
2022-01-26 09:49:04 -0800

    Added random cube generation with three.js

---
+++ 3d-gen/geometry.js
@@ -0,0 +1,95 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+const random = require('canvas-sketch-util/random');
+
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl",
+  attributes: { antialiased: true },
+  
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#000", 1);
+
+  // Setup a camera
+  const camera = new THREE.OrthographicCamera();
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+  // Setup a geometry
+  const box = new THREE.BoxGeometry();
+
+  // Cubes appear randomly at different xyz coords
+  for(let i = 0; i < 10; i++) {
+    const mesh = new THREE.Mesh(
+      box,
+      new THREE.MeshBasicMaterial({
+        color: 'orange',
+      })
+    );
+
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1),
+    );
+    mesh.scale.set(
+      random.range(-1, 2),
+      random.range(-4, 3),
+      random.range(-1, 2),
+    );
+
+    mesh.scale.multiplyScalar(0.5);
+    scene.add(mesh);
+  }
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+
+      camera.updateProjectionMatrix();
+      const aspect = viewportWidth / viewportHeight;
+      const zoom = 1.0;
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      camera.near = -100;
+      camera.far = 100;
+
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
